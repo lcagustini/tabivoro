e5de0e99bea70bcef5a7cb30535534b818a83215
lcagustini <torresmon235@gmail.com>
2017-04-12 18:28:30 +0000

    Added tab timer

--- popup.js
+++ popup.js
@@ -3,3 +3,54 @@ $(function() {
      $('#term').text($('#search').val());
   });
 });
+
+$(function(){
+});
+
+chrome.tabs.onActivated.addListener(function(info){
+    //TODO: ID changes when browser closes
+    for(let tab in globalTabs){
+        globalTabs[tab].active = false;
+        if(globalTabs[tab].id === info.tabId)
+            globalTabs[tab].active = true;
+    };
+});
+
+//TODO: Updated globalTabs on add and remove
+
+var globalTabs = {};
+
+$(function checkTabs(){
+    chrome.tabs.query({}, function(tabs){
+        for(let tab of tabs){
+            tab.time = 0;
+            globalTabs[tab.url] = tab;
+        };
+        console.log(globalTabs);
+    });
+});
+
+var globalT = 0;
+
+function test(){
+    var d = new Date();
+    var t = d.getTime();
+
+    if(globalT === 0)
+        globalT = t;
+
+    var dt = t - globalT;
+    globalT = t;
+
+    for(let tab in globalTabs){
+        if(!globalTabs[tab].active){
+            globalTabs[tab].time += dt;
+        };
+    };
+
+    $('#term').text("olar")
+
+    setTimeout(test, 1000);
+};
+
+window.onload = test;
