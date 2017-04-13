f01e8f0154c718971c0c9167e90ad3c5a721ba8f
lcagustini <torresmon235@gmail.com>
2017-04-13 00:42:58 +0000

    Moved code to background page

--- background.js
+++ background.js
@@ -1,3 +1,32 @@
-function init(){
+var globalTabs = {};
 
+function init()
+{
+    // initialize tabs
+    //TODO: restore time when browser reopens
+    chrome.tabs.query({}, function(tabs){
+        for(let tab of tabs){
+            tab.time = 0;
+            globalTabs[tab.url] = tab;
+        };
+
+        // then start updating
+        update();
+    });
+};
+
+//TODO: Updated globalTabs on add and remove
+
+chrome.tabs.onActivated.addListener(function(info) {
+    //TODO: handle ID changes when browser closes
+    for(let tab in globalTabs){
+        globalTabs[tab].active = false;
+        if(globalTabs[tab].id === info.tabId)
+            globalTabs[tab].active = true;
+            globalTabs[tab].time = 0;
+    };
+});
+
+function getGlobalTabs(){
+    return globalTabs;
 };
--- popup.js
+++ popup.js
@@ -4,39 +4,12 @@ $(function() {
   });
 });
 
-chrome.tabs.onActivated.addListener(function(info) {
-    //TODO: handle ID changes when browser closes
-    for(let tab in globalTabs){
-        globalTabs[tab].active = false;
-        if(globalTabs[tab].id === info.tabId)
-            globalTabs[tab].active = true;
-            globalTabs[tab].time = 0;
-    };
-});
-
-//TODO: Updated globalTabs on add and remove
-
-var globalTabs = {};
-
-function init()
-{
-    // initialize tabs
-    //TODO: restore time when browser reopens
-    chrome.tabs.query({}, function(tabs){
-        for(let tab of tabs){
-            tab.time = 0;
-            globalTabs[tab.url] = tab;
-        };
-
-        // then start updating
-        update();
-    });
-};
-
 var globalT = 0;
 
 function update()
 {
+    var globalTabs = chrome.extension.getBackgroundPage().getGlobalTabs();
+
     console.log(chrome.extension.getBackgroundPage());
     var d = new Date();
     var t = d.getTime();
