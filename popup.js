d807d98779bbb415d20e5b8b23e995876cee9c44
Italo Nicola <italo.niccola@gmail.com>
2017-04-13 01:50:33 +0000

    fix front end again

--- popup.js
+++ popup.js
@@ -18,19 +18,19 @@ function update()
 
     for(let tab in globalTabs) {
         let el = document.createElement('label');
-        el.style = "float: left; white-space: nowrap; overflow: hidden; width: 100%; height: 25px";
+        el.style = "float: left; white-space: nowrap; overflow: hidden; width: 100%; height: 20px; margin-bottom: 5px;";
         let textNode = document.createTextNode(globalTabs[tab].title);
         el.appendChild(textNode);
         urls.appendChild(el);
 
         el = document.createElement('label');
         let icon_url = globalTabs[tab].favIconUrl;
-        if (icon_url) el.style = "background-image: url(" + icon_url + "); float: left; white-space: nowrap; overflow: hidden; width: 100%; height: 25px; background-size: 22px; background-repeat: no-repeat;";
-        else el.style = "float: left; white-space: nowrap; overflow: hidden; width: 100%; height: 25px";
+        if (icon_url) el.style = "background-position: top; background-image: url(" + icon_url + "); float: left; white-space: nowrap; overflow: hidden; width: 100%; height: 20px; background-size: 20px; background-repeat: no-repeat; margin-bottom: 5px;";
+        else el.style = "float: left; white-space: nowrap; overflow: hidden; width: 100%; height: 20px; margin-bottom: 5px;";
         icons.appendChild(el);
 
         el = document.createElement('label');
-        el.style = "text-align: right; float: right; white-space: nowrap; overflow: hidden; width: 100%; height: 25px";
+        el.style = "text-align: right; float: right; white-space: nowrap; overflow: hidden; width: 100%; height: 20px; margin-bottom: 5px;";
         textNode = document.createTextNode(Math.round(globalTabs[tab].time));
         el.appendChild(textNode);
         times.appendChild(el);
